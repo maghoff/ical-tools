031430d3053b3f64817e0555b42f2771945d7521
Magnus Hovland Hoff <maghoff@gmail.com>
2024-06-14 09:47:32 +0200

    Scribble some docs

--- ical-syntax/src/lib.rs
+++ ical-syntax/src/lib.rs
@@ -1,3 +1,6 @@
-//! https://www.ietf.org/rfc/rfc5545.txt
+//! Implemented according to https://www.ietf.org/rfc/rfc5545.txt
+//!
+//! iCalendar is extensible, many of the concrete details are registered at
+//! IANA: https://www.iana.org/assignments/icalendar/icalendar.xhtml
 
 pub mod generate;
