a7629f3314465a38cba1ee104f3dda234591a391
Magnus Hovland Hoff <maghoff@gmail.com>
2024-06-14 19:45:37 +0200

    Fix rustdoc

--- ical-syntax/src/lib.rs
+++ ical-syntax/src/lib.rs
@@ -1,6 +1,6 @@
-//! Implemented according to https://www.ietf.org/rfc/rfc5545.txt
+//! Implemented according to <https://www.ietf.org/rfc/rfc5545.txt>
 //!
 //! iCalendar is extensible, many of the concrete details are registered at
-//! IANA: https://www.iana.org/assignments/icalendar/icalendar.xhtml
+//! IANA: <https://www.iana.org/assignments/icalendar/icalendar.xhtml>
 
 pub mod generate;
